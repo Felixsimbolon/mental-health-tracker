0e836608d69ada5892b6f869ad671c1320e9d6e5
Felixsimbolon <felixsmbln25@gmail.com>
2024-08-28 15:18:40 +0700

    commit 6

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "https://pbp.cs.ui.ac.id/jeremi.felix/mentalhealthtracker"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "jeremi-felix-mentalhealthtracker.pbp.cs.ui.ac.id"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
