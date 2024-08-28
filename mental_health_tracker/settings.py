36247d8c13e30cd0588bddb725021942af20d9c4
Felixsimbolon <felixsmbln25@gmail.com>
2024-08-28 14:40:24 +0700

    commit 3

--- mental_health_tracker/settings.py
+++ mental_health_tracker/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "<URL deployment PWS kamu>"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
